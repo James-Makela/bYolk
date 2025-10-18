61fe6ac296cda8c45521d885d9287ec66c0afbe0
James Makela <jamesilmarimakela@outlook.com>
2025-10-18 16:19:58 +0800

    Add transaction app and define transaction Model

---
+++ apps/transaction/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from apps.core import User
+
+
+# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    vendor = models.CharField(max_length=255)
+    purchase_type = models.CharField(max_length=255)
+    receipt_number = models.CharField(max_length=255)
+    credit = models.DecimalField(max_digits=10, decimal_places=2)
+    debit = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
