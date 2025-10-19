ec6e3d844bf12292cac574eca568ee935cca28f7
James Makela <jamesilmarimakela@outlook.com>
2025-10-19 18:16:34 +0800

    Add new apps
    Added apps for budget, core, and cost

---
+++ apps/cost/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from apps.core import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class Cost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    frequency = models.DurationField()
+
+    def __str__(self):
+        return self.name
