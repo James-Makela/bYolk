d23f186b4da289d93a57bdf981e186f70e18f3de
James Makela <jamesilmarimakela@outlook.com>
2025-10-22 15:21:21 +0800

    Fix tabs, add django-stubs package

--- shell.nix
+++ shell.nix
@@ -6,10 +6,11 @@
 with pkgs;
 mkShell {
   packages = packages ++ [
-    python3.pkgs.pytest
+  python3.pkgs.pytest
   python3.pkgs.mypy
-		python3.pkgs.django
-    ruff
-    direnv
+  python3.pkgs.django
+  ruff
+  direnv
+  python3.pkgs.django-stubs
   ];
 }
