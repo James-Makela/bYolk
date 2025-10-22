aac5b1c4fd71794f9fbc40263a3c17d431cf6004
James Makela <jamesilmarimakela@outlook.com>
2025-10-22 15:23:15 +0800

    Start basic budet class

--- apps/budget/admin.py
+++ apps/budget/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.budget.models import Budget
 
 # Register your models here.
+admin.site.register(Budget)
--- apps/budget/models.py
+++ apps/budget/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from apps.core.models import User
+
 
 # Create your models here.
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    anchor_date = models.DateField()
+    budget_period = models.DurationField()
