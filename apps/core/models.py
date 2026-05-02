42b5e7d5be5eb71ad649c0a503166e860a7330e2
James Makela <jamesilmarimakela@outlook.com>
2026-05-02 16:51:02 +0800

    Refactor categories to app core

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,6 +1,17 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
--- apps/cost/models.py
+++ apps/cost/models.py
@@ -1,19 +1,8 @@
 from django.db import models
-from apps.core.models import User
+from apps.core.models import User, Category
 from datetime import date
 
 from apps.cost.managers import CostQuerySet
-
-
-# TODO: Category needs to be its own thing so it can be a foreign key for both costs and transactions
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    class Meta:
-        verbose_name_plural = "Categories"
-
-    def __str__(self):
-        return f"{self.name}"
 
 
 # Create your models here.
