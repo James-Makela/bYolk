16b70d98994ecc5d39dc833db244b5d95dbb0ed9
James Makela <jamesilmarimakela@outlook.com>
2026-05-02 16:49:18 +0800

    Add features to budget
    Set budget to be sorted by start_date, implemented viewing relevant
    transactions under a set budget period.

--- apps/budget/views.py
+++ apps/budget/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 from apps.budget.filters import BudgetPeriodFilter
 from apps.budget.models import BudgetPeriod, CostAllocation
+from apps.transaction.models import Transaction
 from django.http import HttpResponse, Http404
 
 
@@ -9,7 +10,7 @@ from django.http import HttpResponse, Http404
 def budgets_list(request):
     budget_filter = BudgetPeriodFilter(
         request.GET,
-        queryset=BudgetPeriod.objects.filter(user=request.user)
+        queryset=BudgetPeriod.objects.filter(user=request.user).order_by('-start_date')
     )
     context = {'filter': budget_filter}
     return render(request, 'budget/index.html', context)
@@ -18,7 +19,13 @@ def budgets_list(request):
 def budget_detail(request, budget_id):
     budget = get_object_or_404(BudgetPeriod, pk=budget_id, user=request.user)
     allocations = CostAllocation.objects.filter(budget_period=budget)
+    matching_transactions = Transaction.objects.filter(
+        user=request.user,
+        date__range=[budget.start_date, budget.end_date]
+    ).exclude(purchase_type__iexact="Internal Transfer").order_by('-date')
+
     return render(request, "budget/detail.html", {
         "budget": budget,
         "allocations": allocations,
+        "matching_transactions": matching_transactions,
     })
