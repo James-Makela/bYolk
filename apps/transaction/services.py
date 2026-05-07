d6733775cab412af5b6fad2164bdfa2a1002d7e1
James Makela <jamesilmarimakela@outlook.com>
2026-05-07 13:46:41 +0800

    Add a date override to the transaction import processe
    This allows tracking of when money is spent rather than processed.
    Also added the hashed user id to the generate unique hash function.

--- apps/transaction/services.py
+++ apps/transaction/services.py
@@ -1,18 +1,20 @@
 import re
+from datetime import datetime
 
 import pandas as pd
 
 from .models import Transaction
 
 
-def generate_unique_hash(description, amount, balance):
+def generate_unique_hash(description, amount, balance, user_id):
     receipt_number = re.findall(r"(?<=Receipt )\d{4,6}", description)
     stripped_amount = str(amount).replace(".", "_")
     stripped_balance = str(balance).replace(".", "_")
+    hashed_user = hash(user_id)
     if not receipt_number:
-        return f"000000_{stripped_amount}{stripped_balance}"
+        return f"000000_{stripped_amount}{stripped_balance}{hashed_user}"
     else:
-        return f"{receipt_number[0]}_{stripped_amount}"
+        return f"{receipt_number[0]}_{stripped_amount}{hashed_user}"
 
 
 def process_description(description):
@@ -38,6 +40,24 @@ def process_description(description):
     return vendor, purchase_type, receipt_details
 
 
+def get_actual_date(description):
+    """
+    Works out if there is a transaction date in the description,
+    if so, this should be the transaction date, rather than the processed date.
+    This helps us to keep track of spending when the money is spent rather than
+    processed.
+    """
+    actual_date = re.findall(
+        r"(\d+)[\s]+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[\s]+(\d{4})",
+        description,
+    )
+    if actual_date and len(actual_date[0]) == 4:
+        try:
+            return datetime.strptime(actual_date[0][0], "%d %b %Y").date()
+        except Exception:
+            return None
+
+
 def process_transaction_upload(user, csv_file):
     df = pd.read_csv(csv_file)
     df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
@@ -45,8 +65,11 @@ def process_transaction_upload(user, csv_file):
     transactions_to_create = []
     for _, row in df.iterrows():
         amount = row["Credit"] if pd.notnull(row.get("Credit")) else row.get("Debit", 0)
-        hash = generate_unique_hash(row["Description"], amount, row["Balance"])
+        hash = generate_unique_hash(row["Description"], amount, row["Balance"], user.id)
         vendor, purchase_type, receipt_details = process_description(row["Description"])
+        date = get_actual_date(row["Description"])
+        if not date:
+            date = row["Date"].date()
 
         if (
             not Transaction.objects.filter(unique_hash=hash).exists()
@@ -55,7 +78,7 @@ def process_transaction_upload(user, csv_file):
             transactions_to_create.append(
                 Transaction(
                     user=user,
-                    date=row["Date"].date(),
+                    date=date,
                     vendor=vendor,
                     purchase_type=purchase_type,
                     receipt_details=receipt_details,
