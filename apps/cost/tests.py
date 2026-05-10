677fb6363321b4f2d9d829491fdaf29f4793e36b
James Makela <jamesilmarimakela@outlook.com>
2026-05-10 20:40:31 +0800

    Start Cost tests

--- apps/cost/tests.py
+++ apps/cost/tests.py
@@ -1,3 +1,41 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from apps.core.models import Category
+
+from .models import Cost
+
+User = get_user_model()
+
+
+class CostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="test", email="test@test.com", password="testpass123"
+        )
+        Category.objects.create(user=cls.user, name="Housing")
+
+    def test_create_cost(self):
+        cost = Cost.objects.create(
+            frequency_value=1,
+            frequency_unit="weeks",
+            user=self.user,
+            name="Rent",
+            amount=400,
+            category=Category.objects.get(
+                user=self.user,
+                name="Housing",
+            ),
+            start_date=datetime(2026, 1, 6),
+        )
+
+        self.assertIsInstance(cost, Cost)
+        self.assertEqual(cost.name, "Rent")
+        self.assertEqual(cost.frequency_string(), "Every Week")
+        self.assertEqual(cost.user, self.user)
+        self.assertEqual(cost.amount, 400)
+        self.assertEqual(cost.category.name, "Housing")
+        self.assertEqual(cost.start_date, datetime(2026, 1, 6))
