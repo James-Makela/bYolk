ea923b2cfd468f6443734f7950f4ecc625c57fc0
James Makela <jamesilmarimakela@outlook.com>
2026-05-18 08:54:58 +0800

    Remove income export url

--- apps/income/urls.py
+++ apps/income/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("income/", views.income_edit, name="add-income"),
     path("edit/<int:pk>", views.income_edit, name="edit-income"),
     path("delete/<int:pk>/", views.delete_income, name="delete-income"),
-    path("export", views.income_export, name="export-income"),
 ]
