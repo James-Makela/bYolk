f24901479e44779d624e4a6e2532b763a80fad78
James Makela <jamesilmarimakela@outlook.com>
2026-08-16 13:25:35 +0800

    feat: category chart in budget detail
    Displays a breakdown of categories and how much is spent on each.

---
+++ static/js/category_pie_chart.js
@@ -0,0 +1,66 @@
+function renderChart(labels, series, colors) {
+  const options = {
+    chart: { type: 'pie', width: '100%', height: '90%', background: 'transparent', parentHeightOffset: 0 },
+    series: series,
+    labels: labels,
+    colors: colors,
+    dataLabels: {
+      enabled: false,
+      formatter: function(val) {
+        return `${val.toFixed(1)}%`;
+      }
+    },
+    legend: {
+      enabled: true
+    },
+    stroke: {
+      width: 1,
+      colors: ['#000000']
+    },
+    theme: {
+      mode: 'dark'
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          external: {
+            show: true,
+            formatter: function(name, opts) {
+              const amount = opts.w.globals.series[opts.seriesIndex];
+              return `${name}: $${amount.toFixed(2)}`;
+            }
+          }
+        }
+      }
+    },
+    tooltip: {
+      theme: 'dark',
+      fillSeriesColor: false,
+      style: {
+        fontSize: '13px',
+        fontFamily: 'inherit'
+      },
+      onDatasetHover: {
+        highlightDataSeries: true
+      },
+      marker: {
+        show: true
+      },
+      y: {
+        title: {
+          formatter: (seriesName) => `${seriesName}:`
+        },
+        formatter: function (val, opts) {
+          const seriesArr = opts && opts.w ? opts.w.config.series : series;
+          const total = seriesArr.reduce((a, b) => a + b, 0);
+          const percent = total > 0 ? ((val / total) * 100).toFixed(2) : '0.00';
+
+          return `$${val.toFixed(2)} (${percent}%)`;
+        }
+      }
+    }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#category-pie-chart"), options);
+  chart.render();
+}
