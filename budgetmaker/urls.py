f651dfde4780e75f58bde16b107132944de24e2b
James Makela <jamesilmarimakela@outlook.com>
2026-09-12 17:53:50 +0800

    fix(pwa): add icons from lucide

--- budgetmaker/urls.py
+++ budgetmaker/urls.py
@@ -4,8 +4,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include("apps.core.urls")),
     path("", include("pwa.urls")),
+    path("", include("apps.core.urls")),
     path("costs/", include("apps.cost.urls")),
     path("transactions/", include("apps.transaction.urls")),
     path("budgets/", include("apps.budget.urls")),
